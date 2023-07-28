55db04460ba8ac78abe5e2da80a6dd7e32290e15
lrosenth <lukas.rosenthaler@unibas.ch>
2023-07-28 17:39:45 +0200

    read + create basic functionality

--- omaslib/src/propertyclass.py
+++ omaslib/src/propertyclass.py
@@ -1,15 +1,20 @@
-from typing import Union, Set, Optional
+from typing import Union, Set, Optional, Any
 
 from pystrict import strict
 
+from omaslib.src.connection import Connection
+from omaslib.src.helpers.context import Context
 from omaslib.src.helpers.datatypes import QName, AnyIRI, Languages
 from omaslib.src.helpers.omaserror import OmasError
 from omaslib.src.helpers.xsd_datatypes import XsdDatatypes, XsdValidator
+from omaslib.src.model import Model
 
 
 @strict
-class PropertyClass:
+class PropertyClass(Model):
     _property_class_iri: Union[QName, None]
+    _subproperty_of: Union[QName, None]
+    _exclusive_for_class: Union[QName, None]
     _required: Union[bool, None]
     _multiple: Union[bool, None]
     _to_node_iri: Union[AnyIRI, None]
@@ -19,7 +24,10 @@ class PropertyClass:
     _order: int
 
     def __init__(self,
-                 property_iri: Optional[QName] = None,
+                 con: Connection,
+                 property_class_iri: Optional[QName] = None,
+                 subproperty_of: Optional[QName] = None,
+                 exclusive_for_class: Optional[QName] = None,
                  datatype: Optional[XsdDatatypes] = None,
                  to_node_iri: Optional[AnyIRI] = None,
                  required: Optional[bool] = None,
@@ -27,9 +35,12 @@ class PropertyClass:
                  languages: Optional[Set[Languages]] = None,
                  unique_langs: Optional[bool] = None,
                  order: Optional[int] = None):
-        if not XsdValidator.validate(XsdDatatypes.QName, property_iri):
+        super().__init__(con)
+        if not XsdValidator.validate(XsdDatatypes.QName, property_class_iri):
             raise OmasError("Invalid format of property IRI")
-        self._property_class_iri = property_iri
+        self._property_class_iri = property_class_iri
+        self._subproperty_of = subproperty_of
+        self._exclusive_for_class = exclusive_for_class
         self._datatype = datatype
         self._to_node_iri = to_node_iri
         self._required = required
@@ -42,6 +53,10 @@ class PropertyClass:
         required = '✅' if self._required else '❌'
         multiple = '✅' if self._multiple else '❌'
         propstr = f'Property: {str(self._property_class_iri)};'
+        if self._subproperty_of:
+            propstr += f' Subproperty of {self._subproperty_of};'
+        if self._exclusive_for_class:
+            propstr += f' Exclusive for {self._exclusive_for_class};'
         if self._to_node_iri:
             propstr += f' Datatype: => {self._to_node_iri});'
         else:
@@ -50,12 +65,64 @@ class PropertyClass:
         if self._languages:
             propstr += ' Languages: { '
         for lang in self._languages:
-            propstr += str(lang.value) + ' '
+            propstr += f'"{lang.value}" '
         if self._languages:
             propstr += '};'
         if self._order:
             propstr += f' Order: {self._order}'
+
         return propstr
+
+    @property
+    def property_class_iri(self) -> QName:
+        return self._property_class_iri
+
+    @property_class_iri.setter
+    def property_class_iri(self, value: Any):
+        OmasError(f'property_iri_class cannot be set!')
+
+    def read_owl(self):
+        context = Context(name=self._con.context_name)
+        query1 = context.sparql_context
+        query1 += f"""
+        SELECT ?p ?o
+        FROM {self._property_class_iri.prefix}:onto
+        WHERE {{
+            {self._property_class_iri} ?p ?o
+        }}
+        """
+        res = self._con.rdflib_query(query1)
+        print(self._property_class_iri)
+        datatype = None
+        to_node_iri = None
+        data_prop = False
+        obj_prop = False
+        for r in res:
+            pstr = str(context.iri2qname(r[0]))
+            if pstr == 'owl:DatatypeProperty':
+                data_prop = True
+            elif pstr == 'owl:ObjectProperty':
+                obj_prop = True
+            elif pstr == 'owl:subPropertyOf':
+                self._subproperty_of = r[1]
+            elif pstr == 'rdfs:range':
+                o = context.iri2qname(r[1])
+                if o.prefix == 'xsd':
+                    datatype = o
+                else:
+                    to_node_iri = o
+            elif pstr == 'rdfs:domain':
+                o = context.iri2qname(r[1])
+                self._exclusive_for_class = o
+        # Consistency checks
+        if data_prop and obj_prop:
+            OmasError("Property may not be data- and object-property at the same time")
+        if data_prop:
+            if datatype != self._datatype:
+                OmasError(f'Property has inconstent data type definition: OWL: {datatype} vs SHACL: {self._datatype}.')
+        if obj_prop:
+            if to_node_iri != self._to_node_iri:
+                OmasError(f'Property has inconstent object type definition: OWL: {to_node_iri} vs SHACL: {self._to_node_iri}.')
 
     def to_sparql_insert(self, indent: int = 0) -> str:
         blank = ' '
--- omaslib/src/resourceclass.py
+++ omaslib/src/resourceclass.py
@@ -13,30 +13,28 @@ from omaslib.src.propertyclass import PropertyClass
 
 @strict
 class ResourceClass(Model):
-    _shape: Union[QName, None]
+    #_shape: Union[QName, None]
     _owl_class: Union[QName, None]
-    _subshape_of: Union[QName, None]
+    _subclass_of: Union[QName, None]
     _properties: List[PropertyClass]
     _closed: bool
 
     def __init__(self,
                  con: Connection,
-                 shape: Optional[QName] = None,
                  owl_cass: Optional[QName] = None,
-                 subshape_of: Optional[QName] = None,
+                 subclass_of: Optional[QName] = None,
                  properties: Optional[List[PropertyClass]] = None,
                  closed: Optional[bool] = None) -> None:
         super().__init__(con)
-        self._shape = shape
         self._owl_class = owl_cass
-        self._subshape_of = subshape_of
-        self._properties = properties if properties else []
+        self._subclass_of = subclass_of
+        self._properties = properties if properties else set()
         self._closed = True if closed is None else closed
 
     def __str__(self):
         blank = ' '
         indent = 4
-        s = f'Shape: {self._shape}\nProperties:\n'
+        s = f'Shape: {self._owl_class}Shape\nProperties:\n'
         for p in self._properties:
             s += f'{blank:{indent}}{str(p)}\n'
         return s
@@ -56,51 +54,53 @@ class ResourceClass(Model):
         sparql += f'{blank:{indent}}sh:closed {"true" if self._closed else "false"} .\n'
         return sparql
 
-    @classmethod
-    def from_store(cls,
-                   con: Connection,
-                   shape: QName,
-                   context_name: str = DEFAULT_CONTEXT):
-        context = Context(name=con.context_name)
+    def __read_shacl(self) -> None:
+        context = Context(name=self._con.context_name)
+        if not self._owl_class:
+            raise OmasError('ResourceClass mus be created with "owl_class" given as parameter!')
         query1 = context.sparql_context
         query1 += f"""
         SELECT ?p ?o
-        FROM {shape.prefix}:shacl
+        FROM {self._owl_class.prefix}:shacl
         WHERE {{
-            BIND({str(shape)} AS ?shape)
+            BIND({str(self._owl_class)}Shape AS ?shape)
             ?shape ?p ?o
             FILTER(?p != sh:property)
         }}
         """
         res = con.rdflib_query(query1)
-        owl_class = None
-        subshape_of = None
+        self._subclass_of = None
         target_class = None
-        closed = True
+        self._closed = True
         for r in res:
             p = context.iri2qname(r[0])
             if p == 'rdf:type':
-                if context.iri2qname(r[1]) == f'{shape}':
+                tmp_qname = context.iri2qname(r[1])
+                if tmp_qname == f'{self._owl_class}' or tmp_qname == 'sh:nodeShape':
                     continue
                 else:
-                    owl_class = context.iri2qname(r[1])
+                    raise OmasError(f'Inconsistent Shape for "{self._owl_class}": rdf:type="{context.iri2qname(r[1])}"')
             elif p == 'rdfs:subClassOf':
-                subshape_of = context.iri2qname(r[1])
+                tmpstr = context.iri2qname(r[1])
+                i = tmpstr.find('Shape')
+                if i == -1:
+                    raise OmasError('Shape not valid......')  # TODO: Correct error message
+                self._subclass_of = tmpstr[:i]
             elif p == 'sh:targetClass':
                 target_class = context.iri2qname(r[1])
             elif p == 'sh:closed':
-                closed = closed = r[1].value
-        if target_class and owl_class and target_class != owl_class:
-            raise OmasError(f'Inconsistent shape "{shape}": sh:targetClass "{target_class}" != rdf:type "{owl_class}"')
-        if not owl_class and target_class:
-            owl_class = target_class
+                self._closed = closed = r[1].value
+        if target_class and self._owl_class and target_class != self._owl_class:
+            raise OmasError(f'Inconsistent shape "{self._owl_class}Shape": sh:targetClass "{target_class}" != rdf:type "{self._owl_class}"')
+        if not self._owl_class and target_class:
+            self._owl_class = target_class
 
         query2 = context.sparql_context
         query2 += f"""
         SELECT ?prop ?p ?o ?pp ?oo
-        FROM {shape.prefix}:shacl
+        FROM {self._owl_class.prefix}:shacl
         WHERE {{
-            BIND({str(shape)} AS ?shape)
+            BIND({str(self._owl_class)}Shape AS ?shape)
             ?shape sh:property ?prop .
             ?prop ?p ?o .
             OPTIONAL {{
@@ -129,7 +129,7 @@ class ResourceClass(Model):
             if r[1].fragment == 'languageIn':
                 if not properties[r[0]].get(p):
                     properties[r[0]][p] = set()
-                properties[r[0]][p].add(r[4].toPython())
+                properties[r[0]][p].add(Languages(r[4].toPython()))
         proplist: List[PropertyClass] = []
         for x, p in properties.items():
             p_iri = None
@@ -176,8 +176,8 @@ class ResourceClass(Model):
                     required = True
                     multiple = False
 
-
-            proplist.append(PropertyClass(property_iri=p_iri,
+            proplist.append(PropertyClass(con=self._con,
+                                          property_class_iri=p_iri,
                                           datatype=p_datatype,
                                           to_node_iri=p_to_class,
                                           required=required,
@@ -185,11 +185,67 @@ class ResourceClass(Model):
                                           languages=p_langs,
                                           unique_langs=p_uniquelang))
 
-        return cls(con=con,
-                   shape=shape,
-                   owl_cass=owl_class,
-                   properties=proplist,
-                   closed=closed)
+        self._properties = proplist
+
+    def __read_owl(self):
+        context = Context(name=self._con.context_name)
+        query1 = context.sparql_context
+        query1 += f"""
+        SELECT ?prop ?p ?o
+        FROM {self._owl_class.prefix}:onto
+        WHERE {{
+   	        omas:OmasProject rdfs:subClassOf ?prop .
+            ?prop ?p ?o .
+            FILTER(?o != owl:Restriction)
+        }}
+        """
+        res = self._con.rdflib_query(query1)
+        propdict = {}
+        for r in res:
+            bnode_id = str(r[0])
+            if not propdict.get(bnode_id):
+                propdict[bnode_id] = {}
+                # Default is no restriction on cardinality
+                propdict[bnode_id]['required'] = False
+                propdict[bnode_id]['multiple'] = True
+            p = context.iri2qname(str(r[1]))
+            pstr = str(p)
+            if pstr == 'owl:onProperty':
+                propdict[bnode_id]['property_iri'] = context.iri2qname(str(r[2]))
+            elif pstr == 'owl:onClass':
+                propdict[bnode_id]['to_node_iri'] = context.iri2qname(str(r[2]))
+            elif pstr == 'owl:minQualifiedCardinality':
+                if r[2].value == 1:
+                    propdict[bnode_id]['required'] = True
+                elif r[2].value == 0:
+                    propdict[bnode_id]['required'] = False
+                else:
+                    print(f'ERROR ERROR ERROR: owl:minQualifiedCardinality invalid: "{r[2].value}"')
+            elif pstr == 'owl:maxQualifiedCardinality':
+                if r[2].value == 1:
+                    propdict[bnode_id]['multiple'] = False
+            elif pstr == 'owl:qualifiedCardinality':
+                if r[2].value != 1:
+                    print(f'ERROR ERROR ERROR: QualifiedCardinality invalid: "{r[2].value}"')
+                else:
+                    propdict[bnode_id]['required'] = True
+                    propdict[bnode_id]['mutiple'] = False
+            elif pstr == 'owl:onDataRange':
+                propdict[bnode_id]['datatype'] = context.iri2qname(str(r[2]))
+            else:
+                print(f'ERROR ERROR ERROR: Unknown restriction property: "{pstr}"')
+        for bn, pp in propdict.items():
+            if pp.get('property_iri') is None:
+                OmasError('Invalid restriction node: No property_iri!')
+            property_iri = pp['property_iri']
+            prop = [x for x in self._properties if x.property_class_iri == property_iri]
+            if len(prop) != 1:
+                OmasError(f'Property "{property_iri}" from OWL has no SHACL definition!')
+            prop[0].read_owl()
+
+    def read(self) -> None:
+        self.__read_shacl()
+        self.__read_owl()
 
     def create(self, indent: int = 4):
         blank = ' '
@@ -197,10 +253,10 @@ class ResourceClass(Model):
         context = Context(name=self._con.context_name)
         sparql = context.sparql_context
         sparql += f'INSERT DATA {{\n'
-        sparql += f'{blank:{indent}}GRAPH {self._shape.prefix}:shacl {{\n'
-        sparql += f'{blank:{2*indent}}{self._shape} a sh:nodeShape, {self._owl_class} ;\n'
-        if self._subshape_of:
-            sparql += f'{blank:{3*indent}}rdfs:subClassOf {self._subshape_of} ; \n'
+        sparql += f'{blank:{indent}}GRAPH {self._owl_class.prefix}:shacl {{\n'
+        sparql += f'{blank:{2*indent}}{self._owl_class}Shape a sh:nodeShape, {self._owl_class} ;\n'
+        if self._subclass_of:
+            sparql += f'{blank:{3*indent}}rdfs:subClassOf {self._subclass_of}Shape ; \n'
         sparql += f'{blank:{3*indent}}sh:targetClass {self._owl_class} ; \n'
         for p in self._properties:
             sparql += f'{blank:{3*indent}}sh:property\n'
@@ -212,24 +268,34 @@ class ResourceClass(Model):
         sparql += f'{blank:{2*indent}}sh:closed {"true" if self._closed else "false"} .\n'
         sparql += f'{blank:{indent}}}}\n'
         sparql += f'}}\n'
-        self._con.update_query(sparql)
+        print(sparql)
+        return
+        #self._con.update_query(sparql)
 
 if __name__ == '__main__':
     con = Connection('http://localhost:7200', 'omas')
-    omas_project = ResourceClass.from_store(con, QName('omas:OmasProjectShape'))
+    omas_project = ResourceClass(con, QName('omas:OmasProject'))
+    omas_project.read()
+    print(str(omas_project))
+    exit(-1)
+    omas_project.create()
+    exit(-1)
     plist = [
-        PropertyClass(property_iri=QName('omas:commentstr'),
+        PropertyClass(con=con,
+                      property_class_iri=QName('omas:commentstr'),
                       datatype=XsdDatatypes.string,
                       languages={Languages.DE, Languages.EN},
                       unique_langs=True,
                       multiple=True,
                       required=True),
-        PropertyClass(property_iri=QName('omas:creator'),
+        PropertyClass(con=con,
+                      property_class_iri=QName('omas:creator'),
                       to_node_iri=QName('omas:User'),
                       multiple=False,
                       required=True,
                       order=2),
-        PropertyClass(property_iri=QName('omas:createdAt'),
+        PropertyClass(con=con,
+                      property_class_iri=QName('omas:createdAt'),
                       datatype=XsdDatatypes.dateTime,
                       multiple=False,
                       required=True,
@@ -237,8 +303,8 @@ if __name__ == '__main__':
     ]
     comment_class = ResourceClass(
         con=con,
-        shape=QName('omas:OmasCommentShape'),
         owl_cass=QName('omas:OmasComment'),
+        subclass_of=QName('OmasUser'),
         properties=plist,
         closed=True
     )
