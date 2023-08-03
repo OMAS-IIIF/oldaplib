79a876bdcf35a4fde0ab8fce875fa854f5675351
lrosenth <lukas.rosenthaler@unibas.ch>
2023-08-03 23:46:31 +0200

    …

--- omaslib/src/resourceclass.py
+++ omaslib/src/resourceclass.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional, List
+from typing import Union, Optional, List, Set, Any, Dict, Tuple
 from pystrict import strict
 from rdflib import URIRef, Literal, BNode
 
@@ -13,11 +13,12 @@ from omaslib.src.propertyclass import PropertyClass, PropertyRestrictions, Prope
 
 @strict
 class ResourceClass(Model):
-    #_shape: Union[QName, None]
     _owl_class: Union[QName, None]
     _subclass_of: Union[QName, None]
     _properties: List[PropertyClass]
     _closed: bool
+
+    _changeset: Set[str]
 
     def __init__(self,
                  con: Connection,
@@ -30,14 +31,58 @@ class ResourceClass(Model):
         self._subclass_of = subclass_of
         self._properties = properties if properties else set()
         self._closed = True if closed is None else closed
+        self._changeset = set()
 
     def __str__(self):
         blank = ' '
         indent = 4
-        s = f'Shape: {self._owl_class}Shape\nProperties:\n'
+        s = f'Shape: {self._owl_class}Shape\n'
+        if self._subclass_of:
+            s += f'Subclass of "{self._subclass_of}"\n'
+        s += f'Closed: {self._closed}\n'
+        s += 'Properties:\n'
         for p in self._properties:
             s += f'{blank:{indent}}{str(p)}\n'
         return s
+
+    @property
+    def owl_class(self) -> QName:
+        return self._owl_class
+
+    @owl_class.setter
+    def owl_class(self, value: Any) -> None:
+        OmasError(f'owl_class cannot be modified/set!')
+
+    @property
+    def subclass_of(self) -> Union[QName, None]:
+        return self._subclass_of
+
+    @subclass_of.setter
+    def subclass_of(self, value: QName) -> None:
+        if value != self._subclass_of:
+            self._changeset.add("subclass_of")
+            self._subclass_of = value
+
+    @property
+    def closed(self) -> bool:
+        return self._closed
+
+    @closed.setter
+    def closed(self, value: bool):
+        if value != self._closed:
+            self._changeset.add("closed")
+            self._closed = value
+
+    def get_property(self, property_class_iri: QName) -> PropertyClass:
+        for p in self._properties:
+            if p.property_class_iri == property_class_iri:
+                return p
+        raise OmasError(f'Property with iri "{property_class_iri}" does not exist.')
+
+    def add_property(self, property: PropertyClass):
+        for p in self._properties:
+            if p.property_class_iri == property.property_class_iri:
+
 
     def to_sparql_insert(self, indent: int) -> str:
         blank = ' '
@@ -306,10 +351,103 @@ class ResourceClass(Model):
         self.__create_shacl()
         self.__create_owl()
 
+    def __update_shacl(self, indent: int = 0, indent_inc: int = 4) -> None:
+        if not self._changeset:
+            return
+        blank = ''
+        context = Context(name=self._con.context_name)
+        sparql = context.sparql_context
+        sparql += f'{blank:{indent*indent_inc}}DELETE {{\n'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}GRAPH {self._owl_class.prefix}:shacl {{\n'
+        for c in self._changeset:
+            if c == 'subclass_of':
+                sparql += f'{blank:{(indent + 2) * indent_inc}}?resclass rdfs:subClassOf ?subclass_of .'
+            elif c == 'closed':
+                sparql += f'{blank:{(indent + 2) * indent_inc}}?resclass sh:closed ?closed .'
+            else:
+                pass  # TODO: ERROR handling
+        sparql += f'{blank:{(indent + 1)*indent_inc}}}}\n'
+        sparql += f'{blank:{indent*indent_inc}}}}\n'
+
+        sparql += f'{blank:{indent*indent_inc}}INSERT {{\n'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}GRAPH {self._owl_class.prefix}:shacl {{\n'
+        for c in self._changeset:
+            if c == 'subclass_of' and self._subclass_of:
+                sparql += f'{blank:{(indent + 2) * indent_inc}}{self._owl_class}Shape rdfs:subClassOf {self._subclass_of} .\n'
+            elif c == 'closed':
+                sparql += f'{blank:{(indent + 3) * indent_inc}}{self._owl_class}Shape sh:closed {"true" if self._closed else "false"} .\n'
+        else:
+            pass  # TODO: Implement proper error handling here
+        sparql += f'{blank:{(indent + 1)*indent_inc}}}}\n'
+        sparql += f'{blank:{indent*indent_inc}}}}\n'
+
+        sparql += f'{blank:{indent*indent_inc}}WHERE {{\n'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}GRAPH {self._owl_class.prefix}:shacl {{\n'
+        sparql += f'{blank:{(indent + 2)*indent_inc}}{self._owl_class}Shape rdf:type sh:nodeShape .\n'
+        for c in self._changeset:
+            if c == 'subclass_of':
+                sparql += f'{blank:{(indent + 2) * indent_inc}}OPTIONAL {{\n'
+                sparql += f'{blank:{(indent + 3)*indent_inc}}{self._owl_class}Shape rdfs:subClassOf ?subclass_of .\n'
+                sparql += f'{blank:{(indent + 2) * indent_inc}}}}\n'
+            elif c == 'closed':
+                sparql += f'{blank:{(indent + 2) * indent_inc}}OPTIONAL {{\n'
+                sparql += f'{blank:{(indent + 3) * indent_inc}}{self._owl_class}Shape sh:closed ?closed .\n'
+                sparql += f'{blank:{(indent + 2) * indent_inc}}}}\n'
+            else:
+                pass  # TODO: Implement proper error handling here
+        sparql += f'{blank:{(indent + 1)*indent_inc}}}}\n'
+        sparql += f'{blank:{indent*indent_inc}}}}\n'
+        self._con.update_query(sparql)
+
+    def __update_owl(self, indent: int = 0, indent_inc: int = 4):
+        if not self._changeset:
+            return
+        blank = ''
+        context = Context(name=self._con.context_name)
+        sparql = context.sparql_context
+        sparql += f'{blank:{indent*indent_inc}}DELETE {{\n'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}GRAPH {self._owl_class.prefix}:onto {{\n'
+        for c in self._changeset:
+            if c == 'subclass_of':
+                sparql += f'{blank:{(indent + 2) * indent_inc}}{self._owl_class} rdfs:subClassOf ?subclass_of .'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}}}\n'
+        sparql += f'{blank:{indent*indent_inc}}}}\n'
+
+        sparql += f'{blank:{indent*indent_inc}}INSERT {{\n'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}GRAPH {self._owl_class.prefix}:shacl {{\n'
+        for c in self._changeset:
+            if c == 'subclass_of' and self._subclass_of:
+                sparql += f'{blank:{(indent + 2) * indent_inc}}{self._owl_class} rdfs:subClassOf {self._subclass_of} .'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}}}\n'
+        sparql += f'{blank:{indent*indent_inc}}}}\n'
+
+        sparql += f'{blank:{indent*indent_inc}}WHERE {{\n'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}GRAPH {self._owl_class.prefix}:shacl {{\n'
+        sparql += f'{blank:{(indent + 2)*indent_inc}}{self._owl_class} rdf:type owl:Class .\n'
+        for c in self._changeset:
+            if c == 'subclass_of':
+                sparql += f'{blank:{(indent + 2) * indent_inc}}OPTIONAL {{\n'
+                sparql += f'{blank:{(indent + 3)*indent_inc}}{self._owl_class} rdfs:subClassOf ?subclass_of .\n'
+                sparql += f'{blank:{(indent + 2) * indent_inc}}}}\n'
+        sparql += f'{blank:{(indent + 1)*indent_inc}}}}\n'
+        sparql += f'{blank:{indent*indent_inc}}}}\n'
+        self._con.update_query(sparql)
+
+
+    def update(self):
+        self.__update_shacl()
+        self.__update_owl()
+
 if __name__ == '__main__':
     con = Connection('http://localhost:7200', 'omas')
     omas_project = ResourceClass(con, QName('omas:OmasProject'))
     omas_project.read()
+    omas_project.closed = False
+    omas_project.update()
+    omas_project2 = ResourceClass(con, QName('omas:OmasProject'))
+    omas_project2.read()
+    print(omas_project2)
+    exit(0)
     #print(omas_project)
     #omas_project.create()
     #exit(-1)
