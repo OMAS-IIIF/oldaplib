899ea8f16854f7b3d393d393fa8f08f9d5a63484
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-01-09 15:49:32 +0100

    Weitere Tests

--- omasrest/restful.py
+++ omasrest/restful.py
@@ -1,10 +1,35 @@
 from flask import Flask
+from markupsafe import escape
 
-restfulserver = Flask(__name__)
+app = Flask(__name__)
 
-@restfulserver.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+@app.route('/')
+def index():
+    return 'Index Page'
+#
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
 
 if __name__ == '__main__':
-    restfulserver.run()
+    app.run()
