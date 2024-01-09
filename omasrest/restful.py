6cb81f47213786d677b097ecbc3f44382a6b9743
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-01-09 16:44:26 +0100

    erstes request handling…
    sehr basic

--- omasrest/restful.py
+++ omasrest/restful.py
@@ -1,35 +1,59 @@
-from flask import Flask
-from markupsafe import escape
+# from flask import Flask
+# from markupsafe import escape
+#
+# app = Flask(__name__)
+#
+# # @app.route("/")
+# # def hello_world():
+# #     return "<p>Hello, World!</p>"
+#
+# @app.route('/')
+# def index():
+#     return 'Index Page'
+# #
+# # @app.route('/hello')
+# # def hello():
+# #     return 'Hello, World'
+#
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return f'User {escape(username)}'
+#
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return f'Post {post_id}'
+#
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#     # show the subpath after /path/
+#     return f'Subpath {escape(subpath)}'
+#
+#
+# if __name__ == '__main__':
+#     app.run()
+
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Index Page'
-#
-# @app.route('/hello')
-# def hello():
-#     return 'Hello, World'
+    if request.method == 'POST':
+        name = request.form['name']
+        if name.lower() == 'kappa':
+            return "OK"
+        else:
+            return "Error: Falscher Name", 400
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
+    # Einfaches HTML-Formular für die GET-Anfrage
+    return '''
+        <form method="post">
+            Name: <input type="text" name="name"><br>
+            <input type="submit" value="Submit">
+        </form>
+    '''
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
