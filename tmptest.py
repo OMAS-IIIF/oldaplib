62c4e78f7d8f2a111d90fcb6641e91306831f32e
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2024-03-12 17:21:16 +0100

    ...

--- tmptest.py
+++ tmptest.py
@@ -7,46 +7,23 @@ from rdflib.namespace import RDF, RDFS, OWL, SKOS, DCTERMS
 from pyshacl import validate
 
 
-class Field(Enum):
-    AN_INT = 'an_int'
-    A_FLOAT = 'a_float'
-    A_STR = 'a_str'
 
-class Gaga:
-    __fields: Dict[Field, int | float |str]
-
-    def __init__(self,
-                 an_int: int,
-                 a_float: float,
-                 a_str: str):
-        self.__fields = {}
-        self.__fields[Field.AN_INT] = an_int
-        self.__fields[Field.A_FLOAT] = a_float
-        self.__fields[Field.A_STR] = a_str
-
-        for field in Field:
-            setattr(Gaga, field.value, property(
-                partial(Gaga.__get_value, field=field),
-                partial(Gaga.__set_value, field=field),
-                partial(Gaga.__del_value, field=field)))
-
-
-
-    def __get_value(self, field: Field) -> int | float | str:
-        return self.__fields[field]
-
-    def __set_value(self, value: int | float | str, field: Field):
-        self.__fields[field] = value
-
-    def __del_value(self, field: Field):
-        del gaga.__fields[field]
-
+def search(search_term: str, data: str) -> int:
+    index = -1
+    for i in range(0, len(data) - len(search_term)):
+        for j in range(0, len(search_term)):
+            if search_term[j] == data[i + j]:
+                if index == -1:
+                    index = i
+            else:
+                index = -1
+                break
+        if index != -1:
+            return index
+    return index
 
 if __name__ == '__main__':
-    gaga = Gaga(an_int=42, a_float=3.141592653589793, a_str='Hello World!?')
-    gugus = Gaga(an_int=4711, a_float=2.7142857142857143, a_str='Hello Kitty')
-    print(gaga.an_int)
-    print(gaga.a_str)
-    print(gugus.a_float)
-    gaga.a_float = 12.12
-    print(gaga.a_float)
+    i = search("gaga", "this is gaga, or not")
+    print("====>", i)
+    j = search("gugus", "this is gaga, or not")
+    print("====>", j)
